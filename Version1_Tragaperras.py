738eef2fa354001f5cc7ec72f5430891f175f9dc
NicolasGuarinAldana <nguarin15336@universidadean.edu.co>
2025-08-30 13:11:57 -0500

    feat: update Version1_Tragaperras.py

--- Version1_Tragaperras.py
+++ Version1_Tragaperras.py
@@ -10,11 +10,11 @@ while True:
     fila_3 =random.choice(Simbolos)
 
     print (fila_1,"       " , fila_2,"        ", fila_3)
-    if fila_1 == fila_2 == fila_3:
+    if fila_1 == fila_2 == fila_3: # Funcion que define si el usuario gano o perdio
         print("felicidades has ganado")
     else:
         print("has perdido")
-    
+    # Opciones para seguir jugando o dejar de jugar
     Seguir_apostando = input ("Escriba \"Si\" si desea seguir apostando, si no escriba \"No\" "      ).strip().lower()
     if Seguir_apostando != "si":
         print ("Gracias por jugar")
